46ae65df4588e53ea4e42ae1918dcf2130e86be2
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-18 15:20:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,5 +50,5 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   grand_total = 0
-  directors_totals(source)
+  if 
 end
