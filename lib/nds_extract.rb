26e5b281b8e51ebfb93c9db8c1e43a10b9e6eb88
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-18 16:19:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,12 +49,10 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  require 'pry'
   grand_total = 0
   director_index = 0
   directors = directors_totals(source)
   list = list.length
-  binding.pry
   while directors_index < list.length do
     name = list[directors_index]
     grand_total
